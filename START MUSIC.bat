54b16d84f9b9663eed3ea7d6dc6d6243e48425f1
njaffe <noah0m0jaffe@gmail.com>
2022-08-30 15:56:07 -0400

    updated html file location to be relative

--- START MUSIC.bat
+++ START MUSIC.bat
@@ -1,1 +1,1 @@
-start msedge -inprivate --kiosk --edge-kiosk-type=public-browsing --window-size="200,300" --window-position="0,0" --bwsi --disable-and-delete-previous-log  --disable-gpu c:\Users\PC\Desktop\MUSICPLAYER2.html
+start msedge -inprivate --kiosk --edge-kiosk-type=public-browsing --window-size="200,300" --window-position="0,0" --bwsi --disable-and-delete-previous-log  --disable-gpu ./MUSICPLAYER.html

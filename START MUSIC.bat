dd48d5a84154bf1024099c8ca71d6c0380aceb03
njaffe <noah0m0jaffe@gmail.com>
2022-12-31 11:54:06 -0500

    Update START MUSIC.bat

--- START MUSIC.bat
+++ START MUSIC.bat
@@ -1,2 +1,2 @@
 start msedge -inprivate --kiosk --edge-kiosk-type=public-browsing --window-size="600,400" --window-position="0,0" --bwsi --disable-and-delete-previous-log  --disable-gpu ./MUSICPLAYER.html
-# alternative: # start msedge --new-window --disable-gpu --app=file:///<PATH_TO_FILE>\MUSICPLAYER.html --window-size="600,400" --window-position="0,0" --disable-extensions -inprivate --kiosk --edge-kiosk-type=public-browsing
+REM: start msedge --new-window --disable-gpu --app=file:///<PATH_TO_FILE>\MUSICPLAYER.html --window-size="600,400" --window-position="0,0" --disable-extensions -inprivate --kiosk --edge-kiosk-type=public-browsing

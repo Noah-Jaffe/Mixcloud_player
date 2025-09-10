24a5b7e4b61c32e724dad3725d33abee543e5277
Noah-Jaffe <noah0m0jaffe@gmail.com>
2025-09-10 11:54:18 -0400

    update songs to use stream.mixcloud.stream

--- songs.js
+++ songs.js
@@ -2,46 +2,46 @@ const PLAYLIST_SONGS = [
   {
     "name": "Carlo Alberto: Electro mix vol. 1",
     "key": "/chalbidj/electro-mix-vol-1/",
-    "src": "http://audio6.mixcloud.com/secure/c/m4a/64/b/3/5/b/57c5-a5d1-473f-b54d-949dff6e5434.m4a?sig=R-_F4l0rfSFMh20-7aQ5BA"
+    "src": "http://stream.mixcloud.stream/secure/c/m4a/64/b/3/5/b/57c5-a5d1-473f-b54d-949dff6e5434.m4a?sig=R-_F4l0rfSFMh20-7aQ5BA"
   },
   {
     "name": "Carlo Alberto: HIS - House Italian Style vol. 1",
     "key": "/chalbidj/his-house-italian-style-vol-1/",
-    "src": "http://audio14.mixcloud.com/secure/c/m4a/64/d/5/8/6/f650-d51a-469e-9a2c-30ee72e8754b.m4a?sig=4c9tFO6sVAqV01rJ_-gz6A"
+    "src": "http://stream.mixcloud.stream/secure/c/m4a/64/d/5/8/6/f650-d51a-469e-9a2c-30ee72e8754b.m4a?sig=4c9tFO6sVAqV01rJ_-gz6A"
   },
   {
     "name": "Carlo Alberto: Italodance 90'-00' Essential mix",
     "key": "/chalbidj/90-and-00-eurodance-essential-mix/",
-    "src": "http://audio1.mixcloud.com/secure/c/m4a/64/f/c/0/f/c917-2494-47bc-8159-0648b18ee547.m4a?sig=ebKhHxM4iYxe969yh_ybIA"
+    "src": "http://stream.mixcloud.stream/secure/c/m4a/64/f/c/0/f/c917-2494-47bc-8159-0648b18ee547.m4a?sig=ebKhHxM4iYxe969yh_ybIA"
   },
   {
     "name": "DjJEM2: Latin Mix",
     "key": "/DjJEM2/djjem-1069-latin-julio-mix-72023/",
-    "src": "http://audio18.mixcloud.com/secure/c/m4a/64/e/8/c/1/c4bf-5cd4-44c1-80b4-70254a0852ae.m4a?sig=9tMY_NU48MZIz-QowJfcaA"
+    "src": "http://stream.mixcloud.stream/secure/c/m4a/64/e/8/c/1/c4bf-5cd4-44c1-80b4-70254a0852ae.m4a?sig=9tMY_NU48MZIz-QowJfcaA"
   },
   {
     "name": "DJ Mighty: Liquid DnB Mix - Vol 61",
     "key": "DJ Mighty/Liquid DnB Mix - Vol 61 - Killing Time",
-    "src": "http://audio1.mixcloud.com/secure/c/m4a/64/5/e/5/8/a23e-6a64-44cc-ac1f-8a56b8c5d01f.m4a?sig=ZZhpCPmrQ5QgUkfuTnbkog"
+    "src": "http://stream.mixcloud.stream/secure/c/m4a/64/5/e/5/8/a23e-6a64-44cc-ac1f-8a56b8c5d01f.m4a?sig=ZZhpCPmrQ5QgUkfuTnbkog"
   },
   {
     "name": "Frankie 3000: 80's - The Party mix",
     "key": "Frankie 3000/80's - The Party mix",
-    "src": "http://audio6.mixcloud.com/secure/c/m4a/64/6/e/e/7/f97c-446a-4479-9c5b-6c5a526adc85.m4a?sig=rovXIX4Rl40-qiNQvhZ3gQ"
+    "src": "http://stream.mixcloud.stream/secure/c/m4a/64/6/e/e/7/f97c-446a-4479-9c5b-6c5a526adc85.m4a?sig=rovXIX4Rl40-qiNQvhZ3gQ"
   },
   {
     "name": "Frankie 3000: 90's Hits - The Best Of 90's vol. 1",
     "key": "Frankie 3000/90's Hits - The Best Of 90's vol. 1",
-    "src": "http://audio11.mixcloud.com/secure/c/m4a/64/4/f/7/a/1ff6-4b75-4442-84bc-e501544103f3.m4a?sig=GpaSC9A7meMN8YPE0MsSvQ"
+    "src": "http://stream.mixcloud.stream/secure/c/m4a/64/4/f/7/a/1ff6-4b75-4442-84bc-e501544103f3.m4a?sig=GpaSC9A7meMN8YPE0MsSvQ"
   },
   {
     "name": "Scotty Fox: Songs You Might Hear At A Supermarket",
     "key": "Scotty Fox/Songs You Might Hear At A Supermarket",
-    "src": "http://audio5.mixcloud.com/secure/c/m4a/64/8/c/b/b/8891-9030-4547-80e9-5fe34f416b89.m4a?sig=OtgNSrmEc47MMtXo-bwbrw"
+    "src": "http://stream.mixcloud.stream/secure/c/m4a/64/8/c/b/b/8891-9030-4547-80e9-5fe34f416b89.m4a?sig=OtgNSrmEc47MMtXo-bwbrw"
   },
   {
     "name": "Scotty Fox: The Rhythm Of The Night",
     "key": "Scotty Fox/The Rhythm Of The Night",
-    "src": "http://audio14.mixcloud.com/secure/c/m4a/64/d/5/3/b/bac1-8e87-4d6e-801e-a0e4b39e74cc.m4a?sig=GkrqC4pV2NY_r3fFZkv4Hw"
+    "src": "http://stream.mixcloud.stream/secure/c/m4a/64/d/5/3/b/bac1-8e87-4d6e-801e-a0e4b39e74cc.m4a?sig=GkrqC4pV2NY_r3fFZkv4Hw"
   }
 ]
